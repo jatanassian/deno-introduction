b3a60d44ca7f674540b0a80c46f490fddba1d501
Julien Atanassian <julien.atanassian@gmail.com>
2020-05-26 20:12:03 -0400

    write a greet.txt file

---
+++ createFile.ts
@@ -0,0 +1,7 @@
+// Create an encoder to use utf-8 encoding
+const encoder = new TextEncoder();
+
+const greetText = encoder.encode("Hello World\nPlaying around with Deno");
+
+// No need to wrap in a function (like in Node) because we have a top level await
+await Deno.writeFile("greet.txt", greetText);
